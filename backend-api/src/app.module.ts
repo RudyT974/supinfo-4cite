59405f5f2457a206411a9395d00b8b713e58b024
Nathan Rodet <rodetnathan.contact@gmail.com>
2023-02-13 16:26:25 +0100

    Initial commit

---
+++ backend-api/src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+      retryDelay: 3000,
+      retryAttempts: 10,
+      autoLoadEntities: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  constructor(private dataSource: DataSource) { }
+}
