f0bd7ed761c7420b97aa87de060e6c0b2ec1092a
Nathan Rodet <rodetnathan.contact@gmail.com>
2023-02-18 16:02:49 +0100

    Initial Commit

--- backend-api/src/app.module.ts
+++ backend-api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
+import { DataSource } from 'typeorm'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './users/users.entity';
 
 
 @Module({
@@ -14,7 +15,7 @@ import { AppService } from './app.service';
       username: 'postgres',
       password: 'password',
       database: 'postgres',
-      entities: [],
+      entities: [User],
       synchronize: true,
       retryDelay: 3000,
       retryAttempts: 10,
@@ -24,6 +25,7 @@ import { AppService } from './app.service';
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {
   constructor(private dataSource: DataSource) { }
 }
---
+++ backend-api/src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) { }
+
+  async findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  async findOne(id: number): Promise<User> {
+    return this.usersRepository.findOneBy({ id });
+  }
+
+  async remove(id: string): Promise<void> {
+    await this.usersRepository.delete(id);
+  }
+
+  async create(user: User): Promise<User> {
+    return this.usersRepository.save(user);
+  }
+
+  async update(user: User): Promise<User> {
+    return this.usersRepository.merge(user);
+  }
+}
